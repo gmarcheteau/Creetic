3999787fc2c2f7972512131bd78ccef60f20035c
gmarcheteau <gregoire.marcheteau@gmail.com>
2017-01-08 18:43:02 +0100

    Update forms.py
    flask.ext.wtf

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask_wtf import FlaskForm
+from flask.ext.wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Email, URL, Length
 
