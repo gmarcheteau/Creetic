fd6fcdebb514b04204149ec1d5e42090ffaca0c1
gmarcheteau <gregoire.marcheteau@gmail.com>
2017-01-08 18:58:00 +0100

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from flask_wtf import FlaskForm
+from flask_wtf import Form
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Email, URL, Length
 
-class EmailPasswordForm(FlaskForm):
+class EmailPasswordForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
 
-class URLForm(FlaskForm):
+class URLForm(Form):
     url = StringField('Image URL', validators=[DataRequired(),URL(require_tld=True,message="That is not a valid link url!")])
