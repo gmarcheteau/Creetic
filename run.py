1109951502e22382128c6b3ef689894b328768da
gmarcheteau <gregoire.marcheteau@gmail.com>
2017-01-09 14:56:12 +0100

    adding chatbot link

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import app
 import os
 port = int(os.getenv('PORT', 5000))
 print "Starting app on port %d" % port
-#app.run(debug=True)
-app.run(debug=False, port=port, host='0.0.0.0')
+app.run(debug=True)
+#app.run(debug=False, port=port, host='0.0.0.0')
