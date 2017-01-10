abd4870391f1826f2f348fe768b450eee4993752
gmarcheteau <gregoire.marcheteau@gmail.com>
2017-01-10 14:30:04 +0100

    now drawing simpler image in multicluster

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ import os
 port = int(os.getenv('PORT', 5000))
 print "Starting app on port %d" % port
 #app.run(debug=True)
-app.run(debug=False, port=port, host='0.0.0.0'#
+app.run(debug=False, port=port, host='0.0.0.0')
