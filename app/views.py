901fe5a9b4558c3cc4fd359b5bc2ccbc5f1dc7b7
gmarcheteau <gregoire.marcheteau@gmail.com>
2017-01-16 14:58:08 +0100

    Update views.py

--- app/views.py
+++ app/views.py
@@ -62,7 +62,7 @@ def getBS_img():
   #REDIS enqueue blocking function
   #imageresponse = imageapp.commentOnImage(imageurl)
   
-  job = q.enqueue(imageapp.commentOnImage,imageurl)
+  job = q.enqueue(imageapp.commentOnImage,imageurl,number_clusters)
   # TODO: how long to wait?
   while not job.result:
     imageresponse = ''
