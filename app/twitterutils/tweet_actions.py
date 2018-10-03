1aef88c05c5a355396938c3b1dfee883a1a6256b
gmarcheteau <gregoire.marcheteau@gmail.com>
2018-10-03 14:22:43 +0200

    Update tweet_actions.py

--- app/twitterutils/tweet_actions.py
+++ app/twitterutils/tweet_actions.py
@@ -41,8 +41,8 @@ def prepareText(*picurl):
   text = ''
   
   if picurl is not None:
-    print "type of picurl: "+type(picurl)
-    picurl = picurl[0]
+    #print "type of picurl: "+type(picurl)
+    #picurl = picurl[0]
     print "picurl: "+picurl
     #randomly send or not link to comment on site (will have less space for BS on tweet)
     if random.randint(1,3)%3 == 0:
